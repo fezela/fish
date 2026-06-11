f0743f8e48ea7834b522a0130fdf5be2ddab7c10
quatre <quatre@mail.com>
2026-06-11 02:09:05 -0400

    made modifications to the board and card models.  I like the way my new keyboard feels, but I feel like the strokes are a little shorter.  However it does feel really light.  The next major change needs to make it so that kanban can be reached from main.go's gateway calls.  This is the most important piece since I need to make it work only via post and get requests. I also need to jadd database support, and start messing with docker.  The issue is I'm not sure what I should do first. I think the kanban being tied to something else makes the most sense since even if i change the way kanban works I'll be able to copy the style for other components of the web application.

--- kanban/models/board.go
+++ kanban/models/board.go
@@ -5,4 +5,32 @@ type Board struct {
 	Doing []Card
 	Done []Card
 }
+
+ func (b *Board) GetColumn(col string) []Card {
+	 switch col {
+	 case "Backlog":
+		 return b.Backlog
+	 case "Doing":
+		 return b.Doing
+	 case "Done":
+		 return b.Done
+	 }
+
+ }
+
+ func (b *Board) sendCard(c Card, destination string) {
+	switch destination {
+	case "Backlog":
+		c.setStatus("Backlog")
+		b.Backlog = append(b.Backlog, c)
+
+	case "Doing":
+		c.setStatus("Doing")
+		b.Doing = append(b.Doing, c)
+	
+	case "Done":
+		c.setStatus("Done")
+		b.Done = append(b.Done, c)
+	}
+}
  
--- kanban/models/card.go
+++ kanban/models/card.go
@@ -1,10 +1,15 @@
 package models 
 
+import "time"
+
 type Card struct {
 	Id int //example: 1
-	Owner string 
+	Owner string
 	Task string
 	Bounty int//this default to 0 allows the card to have various functions mapped to it by a numerical identifier.  It just lets it be a mutable object.
+	Status string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 
 }
 
@@ -16,8 +21,22 @@ func (c Card) GetTask() string {
 	return c.Task
 }
 
+func (c Card) GetStatus() string {
+	return c.Status
+}
+
+func (c Card) setStatus(status string) {
+	c.Status = status
+} 
+
+
 func (c Card) SetBounty(bounty int) {
 	c.Bounty = bounty
 }
+
+func (c Card) setUpdatedAt(){
+	c.UpdatedAt = time.Now()
+}
+
 
 
